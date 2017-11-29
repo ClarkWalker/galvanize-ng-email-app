dd7a3946d6fecb20095d46a54273d7d311a1070f
Clark Walker <303walker@gmail.com>
2017-11-29 14:53:25 -0700

    get to api is working

--- js/message/message.controller.js
+++ js/message/message.controller.js
@@ -3,12 +3,9 @@ angular.module('inboxApp')
 
 function messages($http) {
   const viewModel = this;
-  // api call .messages
-  // viewModel.mail = 0
   $http.get('https://cw-g64-ng-hypermedia-inbox-api.herokuapp.com/api/messages')
   .then((api) => {
-    console.log('api = ', api.data._embedded.messages);
+    // console.log('api = ', api.data._embedded.messages);
     viewModel.mail = api.data._embedded.messages;
   });
-
 }
